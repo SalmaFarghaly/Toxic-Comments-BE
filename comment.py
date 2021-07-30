4e449bef94880d5bc982c987e967b36f3340a882
SalmaFarghaly <59613960+SalmaFarghaly@users.noreply.github.com>
2021-07-30 16:22:39 +0200

    Update comment.py

--- comment.py
+++ comment.py
@@ -6,6 +6,6 @@ Created on Tue Nov 17 21:51:19 2020
 """
 from pydantic import BaseModel
 from typing import List
-# 2. Class which describes Bank Notes measurements
+# 2. Class which describes and validates the data sent in the request (it is expecting array of strings which are the tweets)
 class comment(BaseModel):
     tweets: List[str] 
